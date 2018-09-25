369f487aab47b910c88b7573a51b14b5a8402237
rcastr13 <rcastr13@calstatela.edu>
2018-09-24 23:32:05 -0700

    debugging fileread with drag-and-drop files

--- ScriptFiles/JSscript.js
+++ ScriptFiles/JSscript.js
@@ -1,14 +1,21 @@
+//TODO Figure out WHY `require` is NOT being accepted.
+const converter = require("./Converter.js");
+
 function allowDrop(ev) {
     //prevents file from being uploaded automatically
     ev.preventDefault();
 }
 
-
 function dropped(ev) {
+	//XXX OG
     //console.log("Drop");
-    ev.preventDefault();
-    let data = ev.dataTransfer;
-    let files = data.files;
-    console.log(files[0]);
+    //ev.preventDefault();
+    //let data = ev.dataTransfer;
+    //let files = data.files;
+    //console.log(files[0]);
 
+	//XXX NEW
+	ev.preventDefault()
+	let data = ev.dataTransfer
+	let conv = converter.convert(data)
 }
