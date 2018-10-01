bc94667349cc8ec6ae726b0c4f73baab60fc3a73
rcastr13 <rcastr13@calstatela.edu>
2018-10-01 08:49:37 -0700

    creating directory for .js and .mjs files

---
+++ ScriptFiles/JSscript.mjs
@@ -0,0 +1,24 @@
+//TODO Figure out WHY `require` is NOT being accepted.
+//const converter = require("./Converter.js");
+import {convert} from "converter"
+
+function allowDrop(ev){
+    //prevents file from being uploaded automatically
+    ev.preventDefault();
+}
+
+function dropped(ev){
+	//XXX OG
+    //console.log("Drop");
+    //ev.preventDefault();
+    //let data = ev.dataTransfer;
+    //let files = data.files;
+    //console.log(files[0]);
+
+	//XXX NEW
+	ev.preventDefault()
+	let data = ev.dataTransfer
+	//let conv = converter.convert(data)
+	let conv = convert(data)
+	console.log(conv)
+}
